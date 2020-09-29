f327abab576b3f336544ab8b1f25d0446292ba9b
Leonard Ehrenfried <mail@leonard.io>
2020-09-29 22:39:34 +0200

    Bump aws provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.12"
 
   required_providers {
-    aws    = "~> 2.40"
+    aws    = "~> 3.0"
     http   = "~> 1.2"
     random = "~> 2.2"
     tls    = "~> 2.1"
